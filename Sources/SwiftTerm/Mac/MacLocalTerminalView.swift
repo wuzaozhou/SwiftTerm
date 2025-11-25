3a31b21c9c223ad066bc2942cc7f06ff4b972cb4
Jose <1658922587@qq.com>
2025-11-25 16:50:09 +0800

    Update MacLocalTerminalView.swift

--- Sources/SwiftTerm/Mac/MacLocalTerminalView.swift
+++ Sources/SwiftTerm/Mac/MacLocalTerminalView.swift
@@ -200,7 +200,7 @@ open class LocalProcessTerminalView: TerminalView, TerminalViewDelegate, LocalPr
         }
         feed (byteArray: slice)
         print("dataReceived \(shell)")
-        if (shell.hasPrefix("使用HDTool")) {
+        if (shell.hasPrefix("使用HDTool") || shell.constain("Integratingclientprojects")) {
             isRun = false
         }
     }
